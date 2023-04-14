a83a86f88c9898c6ee1050d86a6a597e5ede2fbb
VickyChen0623 <96273804+VickyChen0623@users.noreply.github.com>
2023-04-14 15:38:34 -0400

    Added Comments

--- src/main/java/jhu/petstore/controller/LoginController.java
+++ src/main/java/jhu/petstore/controller/LoginController.java
@@ -26,13 +26,14 @@ public class LoginController {
     public void login(@RequestBody LoginRequestBody requestBody, HttpServletRequest request, HttpServletResponse response) throws IOException {
         String firstname = loginService.verifyLogin(requestBody.getUserId(), requestBody.getPassword());
 
-        // Create a new session for the user if user ID and password are correct, otherwise return Unauthorized error.
+        // Create a new session for the user to verify username and password return Unauthorized error if username/password is unexpected.
         if (!firstname.isEmpty()) {
-            // Create a new session, put user ID as an attribute into the session object, and set the expiration time to 600 seconds.
+            // Create a new session, put user ID as an attribute into the session object, and set the expiration time to 60 mins.
             HttpSession session = request.getSession();
             session.setAttribute("user_id", requestBody.getUserId());
-            session.setMaxInactiveInterval(600);
+            session.setMaxInactiveInterval(60*60);
 
+            //return userid and firstname as response
             LoginResponseBody loginResponseBody = new LoginResponseBody(requestBody.getUserId(), firstname);
             response.setContentType("application/json;charset=UTF-8");
             response.getWriter().print(new ObjectMapper().writeValueAsString(loginResponseBody));
